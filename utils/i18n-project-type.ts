593999759c511d00c0fbcab1cc26a699a99c9dc8
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-08-26 15:50:33 +0200

    Improve message IDs
    `.singular` and `.plural` at the end of the singular type make for a
    better ID.

--- utils/i18n-project-type.ts
+++ utils/i18n-project-type.ts
@@ -1,50 +1,50 @@
 const projectTypeMessages = defineMessages({
   datapack: {
-    id: 'project-type.datapack',
+    id: 'project-type.datapack.singular',
     defaultMessage: 'Data Pack',
   },
   datapacks: {
-    id: 'project-type.datapacks',
+    id: 'project-type.datapack.plural',
     defaultMessage: 'Data Packs',
   },
   mod: {
-    id: 'project-type.mod',
+    id: 'project-type.mod.singular',
     defaultMessage: 'Mod',
   },
   mods: {
-    id: 'project-type.mods',
+    id: 'project-type.mod.plural',
     defaultMessage: 'Mods',
   },
   modpack: {
-    id: 'project-type.modpack',
+    id: 'project-type.modpack.singular',
     defaultMessage: 'Modpack',
   },
   modpacks: {
-    id: 'project-type.modpacks',
+    id: 'project-type.modpack.plural',
     defaultMessage: 'Modpacks',
   },
   plugin: {
-    id: 'project-type.plugin',
+    id: 'project-type.plugin.singular',
     defaultMessage: 'Plugin',
   },
   plugins: {
-    id: 'project-type.plugins',
+    id: 'project-type.plugin.plural',
     defaultMessage: 'Plugins',
   },
   resourcepack: {
-    id: 'project-type.resourcepack',
+    id: 'project-type.resourcepack.singular',
     defaultMessage: 'Resource Pack',
   },
   resourcepacks: {
-    id: 'project-type.resourcepacks',
+    id: 'project-type.resourcepack.plural',
     defaultMessage: 'Resource Packs',
   },
   shader: {
-    id: 'project-type.shader',
+    id: 'project-type.shader.singular',
     defaultMessage: 'Shader',
   },
   shaders: {
-    id: 'project-type.shaders',
+    id: 'project-type.shader.plural',
     defaultMessage: 'Shaders',
   },
 })
