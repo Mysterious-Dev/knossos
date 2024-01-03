af35e875ff0f446cef8ecdaa609025c9868e2305
Sasha Sorokin <10401817+brawaru@users.noreply.github.com>
2024-01-03 10:56:41 +0100

    Fix createdAgoLabel message use

--- pages/settings/sessions.vue
+++ pages/settings/sessions.vue
@@ -39,7 +39,7 @@
             "
           >
             {{
-              formatMessage(commonMessages.createdAtLabel, {
+              formatMessage(commonMessages.createdAgoLabel, {
                 ago: formatRelativeTime(session.created),
               })
             }}
